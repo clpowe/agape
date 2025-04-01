e18fe98b16c0f7bfb31a3fa5a9e75371a204bdc3
clpowe <clpowe@gmail.com>
2025-03-31 21:09:30 -0400

    update typography

--- app/components/AppTypography.vue
+++ app/components/AppTypography.vue
@@ -36,48 +36,85 @@ const variantClass = computed(() => {
 </template>
 
 <style scoped>
+/* TEXT SIZES */
+.text-small {
+  font-size: 1rem;
+  /* 16px */
+  font-family: Almarai, sans-serif;
+}
+
+.text-medium {
+  font-size: 1.333rem;
+  /* ~21.3px */
+  font-family: Almarai, sans-serif;
+}
+
+.text-large {
+  font-size: 1.777rem;
+  /* ~28.4px */
+  font-family: Almarai, sans-serif;
+  text-wrap: balance;
+}
+
+.text-xl {
+  font-size: 2.369rem;
+  /* ~37.9px */
+  font-family: Almarai, sans-serif;
+}
+
+/* FIXED HEADING SIZES */
+.heading-small {
+  font-size: 1.333rem;
+  line-height: 1.4;
+  font-family: var(--font-crimson);
+}
+
+.heading {
+  font-size: 1.777rem;
+  line-height: 1.3;
+  font-family: var(--font-crimson);
+}
+
+.heading-medium {
+  font-size: 2.369rem;
+  line-height: 1.2;
+  font-family: var(--font-crimson);
+}
+
+.heading-large {
+  font-size: 3.157rem;
+  line-height: 1.1;
+  font-family: var(--font-crimson);
+}
+
+.header2 {
+  font-size: 4.209rem;
+  line-height: 1;
+  font-family: var(--font-crimson);
+}
+
+.header2-lg {
+  font-size: 5.61rem;
+  line-height: 1;
+  font-family: var(--font-crimson);
+}
+
+/* FLUID VARIABLE HEADINGS */
+.heading-variable-small {
+  font-size: clamp(1rem, 0.2rem + 1.2vw, 1.333rem);
+  font-family: var(--font-crimson);
+}
+
+.heading-variable {
+  font-size: clamp(1.333rem, 0.5rem + 2.5vw, 2.369rem);
+  line-height: 0.9;
+  font-family: var(--font-crimson);
+}
+
 .heading-variable-large {
   font-size: clamp(3.25rem, 1px + 4.5vw, 5.375rem);
   line-height: 100%;
   font-family: var(--font-crimson);
   text-wrap: balance;
 }
-/* .header2 {
-		@apply text-5xl font-gilda text-balance mb-4 leading-tight;
-	}
-
-	.header2-lg {
-		@apply text-4xl md:text-6xl font-gilda text-balance mb-4 leading-tight;
-	}
-
-	.heading-variable-large {
-		@apply text-h1clamp leading-none font-crimson text-balance font-light;
-	}
-*/
-.heading-variable {
-  font-size: clamp(3.25rem, 1px + 3.5vw, 5.375rem);
-  line-height: 0.9;
-  font-family: var(--font-crimson);
-}
-.text-large {
-  font-size: 1.375rem;
-  text-wrap: balance;
-  font-family: Almarai, sans-serif;
-}
-/*
-	.heading-small {
-		@apply text-xl md:text-3xl font-crimson text-balance leading-tight font-light;
-	}
-
-	.heading {
-		@apply text-5xl font-crimson text-balance leading-tight font-light;
-	}
-
-	.heading-medium {
-		@apply text-3xl md:text-3xl font-crimson text-balance leading-tight font-light;
-	}
-
-	.heading-large {
-		@apply text-4xl md:text-6xl font-crimson text-balance leading-tight font-light;
-	} */
 </style>
