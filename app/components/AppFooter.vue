40347e29454189b35241799e20e994acf5f0abb6
clpowe <clpowe@gmail.com>
2025-06-01 23:02:46 -0400

    added footer links

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+const { data: items } = await useAsyncData("navigation-items", () => {
+  return queryCollection("navigation").all();
+});
+</script>
+
 <template>
-  <footer> Footer </footer>
+  <footer class="section wrapper" data-width="wide">
+    <div class="flex">
+      <nav>
+        <ul>
+          <li v-for="item in items" :key="item.id">
+            <NuxtLink :to="item.to">
+              {{ item.label }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li v-for="item in items?.find((i) => i.label === 'services')?.children" :key="item.label">
+            <NuxtLink :to="item.to">
+              {{ item.label }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
 </template>
+
+<style scoped>
+footer {
+  background-color: var(--color-secondary-950);
+  color: var(--color-secondary-50);
+}
+</style>
