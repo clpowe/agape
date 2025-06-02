aa6d41ce0cdff05fe964c08f3c208d6418623c82
clpowe <clpowe@gmail.com>
2025-06-02 06:03:40 -0400

    refactor footer layout and improve accessibility with ARIA labels

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -5,32 +5,51 @@ const { data: items } = await useAsyncData("navigation-items", () => {
 </script>
 
 <template>
-  <footer class="section wrapper" data-width="wide">
-    <div class="flex">
-      <nav>
-        <ul>
-          <li v-for="item in items" :key="item.id">
-            <NuxtLink :to="item.to">
-              {{ item.label }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li v-for="item in items?.find((i) => i.label === 'services')?.children" :key="item.label">
-            <NuxtLink :to="item.to">
-              {{ item.label }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
+  <footer class="">
+    <div class="footer section wrapper" data-width="wide">
+      <div class="flex flex-wrap">
+        <nav aria-labelledby="quick-links">
+          <AppTypography id="quick-links" tag="h3" variant="heading-xxs">Quick Links</AppTypography>
+          <ul>
+            <li v-for="item in items" :key="item.id">
+              <NuxtLink :to="item.to">
+                {{ item.label }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+        <nav aria-labelledby="services-links">
+          <AppTypography id="services-links" tag="h3" variant="heading-xxs">Services</AppTypography>
+          <ul>
+            <li v-for="item in items?.find((i) => i.label === 'services')?.children" :key="item.label">
+              <NuxtLink :to="item.to">
+                {{ item.label }}
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <AppTypography tag="h3" variant="heading-xxs">Contact</AppTypography>
+          <address>
+            P.O. Box 26586, Jacksonville, Florida 32226<br />
+            <a href="mailto:agapechristianbarprep@gmail.com">agapechristianbarprep@gmail.com</a>
+            <br />
+            <a href="tel:904-2340-2853">904-2340-2853</a>
+          </address>
+        </div>
+      </div>
+      <p>&copy; {{ new Date().getFullYear() }} Agape Christian Bar Prep</p>
     </div>
   </footer>
 </template>
 
 <style scoped>
 footer {
+  margin-inline: var(--space-m);
+  margin-bottom: var(--space-m);
+}
+
+.footer {
   background-color: var(--color-secondary-950);
   color: var(--color-secondary-50);
 }
