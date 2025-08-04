cdd6af50205609858595f594e4da442f6254f109
clpowe <clpowe@gmail.com>
2025-08-04 15:49:57 -0400

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-11-01',
-	app: {
-		head: {
-			titleTemplate: '%s %separator %siteName',
-			templateParams: {
-				siteName: 'Agape Christian Bar Prep',
-			},
-		},
-	},
-	devtools: { enabled: true },
-	future: {
-		compatibilityVersion: 4,
-	},
+				compatibilityDate: '2024-11-01',
+				app: {
+								head: {
+												titleTemplate: '%s %separator %siteName',
+												templateParams: {
+																siteName: 'Agape Christian Bar Prep',
+												},
+								},
+				},
+				devtools: { enabled: true },
+				future: {
+								compatibilityVersion: 4,
+				},
 
-	experimental: {
-		granularCachedData: true,
-	},
-	modules: [
-		'@nuxtjs/seo',
-		'@nuxt/fonts',
-		'@nuxt/icon',
-		'@nuxt/image',
-		'@nuxt/scripts',
-		'@nuxtjs/robots',
-		'@nuxt/test-utils',
-		'@nuxt/eslint',
-		'@nuxthub/core',
-		'@nuxt/test-utils/module',
-		'@vueuse/nuxt',
-		'nuxt-posthog',
-		'nuxt-svgo',
-		'vue3-carousel-nuxt',
-		'nuxt-posthog',
-		'@nuxt/ui-pro',
-		'@nuxt/content',
-	],
-	css: ['~/assets/css/main.css'],
+				experimental: {
+								granularCachedData: true,
+				},
+				modules: [
+				 '@nuxtjs/seo',
+				 '@nuxt/fonts',
+				 '@nuxt/icon',
+				 '@nuxt/image',
+				 '@nuxt/scripts',
+				 '@nuxtjs/robots',
+				 '@nuxt/test-utils',
+				 '@nuxt/eslint',
+				 '@nuxthub/core',
+				 '@nuxt/test-utils/module',
+				 '@vueuse/nuxt',
+				 'nuxt-posthog',
+				 'nuxt-svgo',
+				 'vue3-carousel-nuxt',
+				 'nuxt-posthog',
+				 '@nuxt/ui-pro',
+				 '@nuxt/content',
+				 'nuxt-csurf',
+				],
+				css: ['~/assets/css/main.css'],
   ui: {
     colorMode: false
   },
 
-	hub: {
+				hub: {
     projectUrl({ env }) {
       if (env === 'preview'){
         return 'https://f3119a7e.agape-67x.pages.dev/'
@@ -52,22 +53,21 @@ export default defineNuxtConfig({
   },
 
   content: {
-		preview: {
-			api: 'https://api.nuxt.studio',
-		},
+								preview: {
+												api: 'https://api.nuxt.studio',
+								},
 },
-	robots: {
-		sitemap: 'https://agapechristianbarprep.com/sitemap.xml',
-	},
-	runtimeConfig: {
-		uiProLicense: '',
+				robots: {
+								sitemap: 'https://agapechristianbarprep.com/sitemap.xml',
+				},
+				runtimeConfig: {
+								uiProLicense: '',
     airtableApiKey: '',
 
-		public: {
-			posthogPublicKey: 'phc_U861r9C5hjKh8CRlC1uGW6NW6OG3yfhZstFtFEOjCxX',
-			posthogHost: 'https://us.i.posthog.com',
-			posthogDefaults: '2025-05-24',
-		},
-	},
+								public: {
+												posthogPublicKey: 'phc_U861r9C5hjKh8CRlC1uGW6NW6OG3yfhZstFtFEOjCxX',
+												posthogHost: 'https://us.i.posthog.com',
+												posthogDefaults: '2025-05-24',
+								},
+				},
 });
-
